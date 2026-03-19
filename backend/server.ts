75b248acf784439702b144c2062f72358e5b5916
Node <ceecsurya.m2025.com>
2026-03-19 22:00:43 +0530

    https://github.com/Surya-M2002/Job_Portal_Platorm.git

--- backend/server.ts
+++ backend/server.ts
@@ -17,8 +17,6 @@ mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/jobPortal", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   })
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
